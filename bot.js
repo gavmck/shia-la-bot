af9dec917ff00c6ef0aec67b09767708b199d643
Gavyn McKenzie <gavyn@etchapps.com>
2016-11-05 13:48:58 +0000

    Greeting on startup, reply to correct channel

--- bot.js
+++ bot.js
@@ -2,6 +2,26 @@ import 'babel-register';
 import { token } from './token';
 import SlackBot from 'slackbots';
 
+// Post as shialabot
+const params = {
+  as_user: true,
+};
+
+const greetings = [
+  'Some people dream of success, while you\'re gonna get up and work hard at it',
+  'Yes you can, just do it',
+  'If you\'re tired for starting over, stop giving up',
+  'Don\'t let your dreams be dreams',
+  'I have a hard time with free time',
+  'My mom is at my house every day, and she nags me about everything, especially hygiene',
+  'I can\'t tell you how many hot dogs I\'ve eaten in my life',
+  'If I have enough money to eat, I\'m good'
+];
+
+const randomGreeting = () => {
+  return greetings[Math.floor(Math.random()*greetings.length)];
+}
+
 // create a bot
 const bot = new SlackBot({
   token,
@@ -9,20 +29,27 @@ const bot = new SlackBot({
 });
 
 bot.on('start', () => {
+  // Send a greeting when we boot up
+  bot.postMessageToChannel('general', randomGreeting(), params);
+});
 
-  bot.on('message', (data) => {
+bot.on('message', (data) => {
+  const { type, text, channel } = data;
 
-    if (data.type === 'message') {
-      const text = data.text;
-      console.log("Text: ", text);
+  console.log(data);
+  console.log(bot.getChannels()._value.channels[0]);
 
-      if (text) {
-        text = text.toLowerCase();
+  if (type === 'message') {
+    console.log("Text: ", text);
 
-        if (text.indexOf('should') > -1) {
-          bot.postMessageToChannel('general', 'DO IT!');
-        }
+    if (text) {
+      if (text.toLowerCase().indexOf('should') > -1) {
+        const channels = bot.getChannels()._value.channels.filter((item) => {
+          return item.id = channel;
+        });
+
+        bot.postMessageToChannel(channels[0].name, 'DO IT!', params);
       }
     }
-  });
+  }
 });
