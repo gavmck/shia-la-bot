4077053c16ef694da46f125ca26910b1459f16e2
Gavyn McKenzie <gavyn@etchapps.com>
2016-11-05 13:03:48 +0000

    Remove botkey from repo

--- bot.js
+++ bot.js
@@ -1,33 +1,18 @@
+var boykey = require('./key').botkey;
+
 var SlackBot = require('slackbots');
 
 // create a bot
 var bot = new SlackBot({
-    token: 'xoxb-97138802375-xRUsZIsmpWRHp8dOnnUmiB5J', // Add a bot https://my.slack.com/services/new/bot and put the token
+    token: botkey,
     name: 'Shia La Bot'
 });
 
 bot.on('start', function() {
-    // more information about additional params https://api.slack.com/methods/chat.postMessage
-    var params = {
-        icon_emoji: ':cat:'
-    };
-
-    // define channel, where bot exist. You can adjust it there https://my.slack.com/services
-    //bot.postMessageToChannel('general', 'DO IT!');
-
-    // define existing username instead of 'user_name'
-    //bot.postMessageToUser('user_name', 'meow!', params);
-
-    // If you add a 'slackbot' property,
-    // you will post to another user's slackbot channel instead of a direct message
-    //bot.postMessageToUser('user_name', 'meow!', { 'slackbot': true, icon_emoji: ':cat:' });
-
-    // define private group instead of 'private_group', where bot exist
-    //bot.postMessageToGroup('private_group', 'meow!', params);
 
     bot.on('message', function(data) {
         // all ingoing events https://api.slack.com/rtm
-        console.log(data);
+        //console.log(data);
 
         if (data.type == 'message') {
             var text = data.text;
